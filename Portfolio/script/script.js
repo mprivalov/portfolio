a5aa0cd0225088b0bbd9b96d66c9c7863d200914
mprivalov <mar4eloprivalov@gmail.com>
2023-12-08 13:13:55 +0200

    Update script.js

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -1,10 +1,6 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const toolbar = document.getElementsByClassName('toolbar')[0]
+const toggleButton = document.querySelector('.toggle-button')
+const toolbar = document.querySelector('.toolbar')
 
 toggleButton.addEventListener('click',() => {
     toolbar.classList.toggle('active')
 });
-
-toolbar.addEventListener('click',() => {
-    toolbar.classList.tool('hide')
-})
