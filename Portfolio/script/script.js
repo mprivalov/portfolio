3a35341856d6cffaac713cae2eb51777b5f5220c
mprivalov <mar4eloprivalov@gmail.com>
2023-12-08 15:31:54 +0200

    upbtn
    upbutton added, js added

--- Portfolio/script/script.js
+++ Portfolio/script/script.js
@@ -4,3 +4,13 @@ const toolbar = document.querySelector('.toolbar')
 toggleButton.addEventListener('click',() => {
     toolbar.classList.toggle('active')
 });
+
+const homeButton = document.querySelector('.home-button')
+
+window.addEventListener('scroll', () => {
+    if(this.scrollY > 200){
+        homeButton.style.display = 'flex';
+    } else {
+        homeButton.style.display = 'none';
+    }
+});
